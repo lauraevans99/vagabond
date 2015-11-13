0d22668e51d746020fca4bf850dcf1f121ec4d85
lauraevans99 <lauraevans99@gmail.com>
2015-11-12 18:30:43 -0800

    post model creation & migration, added background image and welcome page, added paperclip for image uploads

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,6 @@ class User < ActiveRecord::Base
 
 	has_secure_password
 
-	# validates :password, confirmation: true
-	#validates :password_confirmation, presence: true
-	# validates_confirmation_of :password
-
 	def self.confirm(params)
 		@user = User.find_by({email_digest: params[:email_digest]})
 		@user.try(:authenticate, params[:password])
