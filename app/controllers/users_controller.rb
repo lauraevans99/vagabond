ea7f99b4bb79cc8fa0d4f3899ab3d40037edd7b0
Matthew Cowles <mpcowles726@gmail.com>
2015-11-13 13:06:12 -0800

    created profile page with editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,24 @@ class UsersController < ApplicationController
 		@user = User.find(id)
 	end
 
+	def edit
+		id = params[:id]
+		@user = User.find(id)
+
+	end
+
+	def update
+		id = params[:id]
+		@user = User.find(id)
+		if @user.update_attributes(user_params)
+			#handles a successful update
+			redirect_to "/users/#{@user.id}"
+		else 
+			render 'edit'
+		end
+
+	end
+
 	def user_params
 		params.require(:user).permit(:alias, :email_digest, :password, :password_confirmation)
 	end
