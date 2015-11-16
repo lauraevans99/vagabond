bf7c079c2acb97c451e70c267ce0a68f3ebd7566
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2015-11-16 10:05:30 -0800

    main map with all cities

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,6 +1,21 @@
 class CitiesController < ApplicationController
 
 	def index
+		@cities = City.all
+		gon.cities = @cities
+		render :index
+
 	end
 	
+	def show
+		@city = City.find(params[:id])
+		# id = cities[:city_id]
+		if @city
+			
+			render :show
+			# redirect_to "/cities/#{@city.id}/posts"
+		else 
+			redirect_to "/cities"
+		end
+	end
 end
